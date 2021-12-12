1e90d54c9d6ad0d3220dc21947fc068715ee5f13
Aniruddha Prithul <aprithul@gmail.com>
2021-12-12 13:59:04 -0800

    QuadTree with area.. wip

--- App/src/Demo.cpp
+++ App/src/Demo.cpp
@@ -4,18 +4,28 @@
 #include "MathUtil.hpp"
 #include <random>
 #include <vector>
+#include <queue>
 
+#define MAX_LVL 5
+#define PART_COUNT 5
 struct Particle
 {
-    int x,y;
+    float x,y;
     int rad;
+    float velX,velY;
 };
+
+
 
 struct Node
 {
+    static std::queue<Node*> NodePool;
+
     int x,y;
     int w,h;
-    Node(int _x, int _y, int _w, int _h) : x(_x), y(_y), w(_w), h(_h)
+    int lvl;
+
+    Node(int _x, int _y, int _w, int _h, int _lvl) : x(_x), y(_y), w(_w), h(_h), lvl(_lvl)
     {
         
     }
@@ -27,29 +37,56 @@ struct Node
     void Add(Particle _particle)
     {
 
-        if(particles.size() < 4)
+        if(particles.size() < 4 || lvl == MAX_LVL)
         {
             particles.push_back(_particle);
+            //printf("[%d,%d] Added to %d, %d, %d, %d\n", _particle.x, _particle.y, x,y,w,h);
         }
         else
         {
             if(children.size() == 0)
             {
-                children.push_back( new Node(x,y, w/2, h/2));
-                children.push_back( new Node(x+w/2,y, w/2, h/2));
-                children.push_back( new Node(x,y+h/2, w/2, h/2));
-                children.push_back( new Node(x+w/2,y+h/2, w/2, h/2));
+                Node* n1 = NodePool.front();
+                NodePool.pop();
+                Node* n2 = NodePool.front();
+                NodePool.pop();
+                Node* n3 = NodePool.front();
+                NodePool.pop();
+                Node* n4 = NodePool.front();
+                NodePool.pop();
+
+                n1->Set(x,y, w/2, h/2, lvl+1);
+                children.push_back(n1);
+                n2->Set(x+w/2,y, w/2, h/2,lvl+1);
+                children.push_back(n2);
+                n3->Set(x,y+h/2, w/2, h/2,lvl+1);
+                children.push_back(n3);
+                n4->Set(x+w/2,y+h/2, w/2, h/2,lvl+1);
+                children.push_back(n4);
+
+
+                for(auto _p : particles)
+                {
+                    for(auto _node : children)
+                    {
+                        if(_node->Inside(_p))
+                        {
+                            _node->Add(_p);
+                            break;
+                        }
+                    }
+                }
             }
-
-
-           for(auto _node : children)
-           {
+            
+             for(auto _node : children)
+            {
                 if(_node->Inside(_particle))
                 {
                     _node->Add(_particle);
                     break;
                 }
-           }
+            }
+            
         }
         
     }
@@ -57,7 +94,16 @@ struct Node
 
     int Inside(Particle& _particle)
     {
-        if(_particle.x >= x && _particle.x <= x+w && _particle.y >= y && _particle.y <= y+h)
+
+        float _px0 = _particle.x-_particle.rad;
+        float _py0 = _particle.y-_particle.rad;
+        float _px1 = _particle.x+_particle.rad;
+        float _py1 = _particle.y+_particle.rad;
+        
+        
+        if( (_px0 >= x && _px0 <= (x+w)) || (_px1 >= x && _px1 <= (x+w)) &&
+            (_py0 >= y && _py0 <= (y+h)) || (_py1 >= y && _py1 <= (y+h)) )
+        //if(_particle.x >= x && _particle.x <= x+w && _particle.y >= y && _particle.y <= y+h)
             return 1;
         else
             return 0;
@@ -80,14 +126,46 @@ struct Node
         
     }
 
+    void Draw(Canvas& _canvas, Color& _colPoint,  Color& _colGrid)
+    {
+        if(children.size() == 0)
+        {
+            for(auto& p : particles)
+            {
+                _canvas.DrawCircle((int)p.x, (int)p.y, p.rad, _colPoint);
+                //_canvas.DrawPixel((int)p.x, (int)p.y, _colPoint);
+                _canvas.DrawRectangle(x,y,w,h, _colGrid);
+                //printf("[%d, %d, %d, %d] -> [%d, %d]\n", x,y,w,h, p.x, p.y);
+            }
+        }
+        else
+        {
+            for(auto n : children)
+                n->Draw(_canvas, _colPoint, _colGrid);
+        }
+    }
+
+    void Set(int _x, int _y, int _w, int _h, int _lvl)
+    {
+        for(auto c : children)
+            NodePool.push(c);
+        children.clear();
+        particles.clear();
+        x = _x;
+        y = _y;
+        w = _w;
+        h = _h;
+        lvl = _lvl;
+    }
+
 };
 
+std::queue<Node*> Node::NodePool;
 
 
-
-Int_32 h = 512;
-Int_32 w = 512;
-Canvas canvas("Canvas Demo", w, h, 1, false);
+Int_32 h = 128;
+Int_32 w = 128;
+Canvas canvas("Canvas Demo", w, h, 8, false);
 
 Color colors[3] = { Color(0xffff0000), Color(0xff00ff00), Color(0xff0000ff)};
 
@@ -95,31 +173,57 @@ Float_32 rot = 0;
 Mat3x3 rotMat;
 Mat3x3 sclMat;
 Mat3x3 transMat;
+std::vector<Particle> particles;
 
 void init()
 {
 
-    Node* root = new Node(0,0,w,h);
-
-    for(int i=0; i<5; i++)
+    for(int i=0; i<1024; i++)
     {
-        root->Add(Particle{1,1,2});
+        Node::NodePool.push(new Node(0,0,0,0,0));
     }
+    
+    for(int i=0; i<PART_COUNT; i++)
+    {
+        particles.push_back(Particle{ (float)(rand()%w), (float)(rand()%h), 5, (float)(rand()%3)-1, (float)(rand()%3)-1});
+    }
+   
+
 
     // print tree
-    root->Print();
+    
 
     printf("Initialized\n");
-    
+    //root->Print();
 
 }
 
+
+void ConstructQuadTree(Node* root, float DeltaTime)
+{
+    //particles.clear();
+    srand(20);
+    root->Set(0,0,w,h,0);
+
+    
+    for(int i=0; i<PART_COUNT; i++)
+    {
+        //particles[i].x += particles[i].velX*DeltaTime;
+        //particles[i].y += particles[i].velY*DeltaTime;
+        if(particles[i].x + particles[i].velX <= 0 || particles[i].x + particles[i].velX >w)
+            particles[i].velX = -particles[i].velX;
+        if(particles[i].y + particles[i].velY <= 0 || particles[i].y + particles[i].velY >h)
+            particles[i].velY = -particles[i].velY;
+
+        //particles.push_back(Particle{rand()%w, rand()%h, 1,});
+        root->Add(particles[i]);
+    }
+}
 
 int load = 0;
 
 void update()
 {
-
 
     // for(int x=0; x<w; x++)
     // {
@@ -136,7 +240,7 @@ void update()
 
     Color red(0xff, 0,0,0xff, BGRA);
     Color blue(0, 0,0xff,0xff, BGRA);
-    Color green(0, 0,0xff,0xff, BGRA);
+    Color green(0, 0xff,0, 0xff, BGRA);
 
     Vec2f p0(canvas.Width/2,canvas.Height/2);
     Vec2f p1(canvas.Width/1.3,canvas.Height/2);
@@ -170,6 +274,37 @@ void update()
     Image* _img = canvas.GetImageById(_id1);
 
     static Float_32 _x = 0,_y=0;
+
+
+    static Node* root = new Node(0,0,0,0,0);
+
+    ConstructQuadTree(root, canvas.DeltaTime*0.0075f);
+    //root->Draw(canvas, red, blue);
+
+    Node _node1(0,0,w/2, h/2, 0);
+    Node _node2(0,h/2,w/2, h/2, 0);
+    Particle p{20,(float)20,5,0,0};
+
+    canvas.DrawRectangle(0, 0, w/2,h/2,blue);
+    canvas.DrawRectangle(0, h/2, w/2,h/2,blue);
+    
+    if(_node1.Inside(p))
+    {
+        canvas.DrawRectangle(0, 0, w/2,h/2,green);
+    }
+    if(_node2.Inside(p))
+    {
+        canvas.DrawRectangle(0, h/2, w/2,h/2,green);
+    }
+    
+    canvas.DrawCircle(p.x,p.y, p.rad, blue);
+
+
+    // for(auto p : particles)
+    // {
+    //     canvas.DrawPixel(p.x, p.y, red);
+    // }
+
 
     //canvas.BlitImage(_img, _x, _y);
 
